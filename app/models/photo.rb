970dd88c3a16f3afe3946db6c4ba9bb41c2cdc0e
johnson-cole <colejohnson@chicagobooth.edu>
2021-04-21 02:08:04 +0000

    Stopped Photogram Industrial 1 at Sample Data Task

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,9 @@ class Photo < ApplicationRecord
   
   has_many :likes
   has_many :comments
+  has_many :fans, through: :likes
+
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
+
 end
